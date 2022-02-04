db71266a11f62c5191e062e3d9252a2500748641
Youngjun-H <hghyj719@gmail.com>
2022-02-04 15:06:57 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+# print("완성!")
+
+# a = 0.3+ 0.6
+# b= round(a,0)
+
+# c = [0] * 10
+# print(c)
+
+# a = [1, 2, 3, 4, 5, 6, 7]
+# print(a[0])
+
+array = [i for i in range(20) if i % 2 == 1]
+
+print(array)
